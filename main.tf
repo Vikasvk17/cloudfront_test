06012fd70bf3e292fd344d29e001a4cc49d6d5ab
Vikas Vishwakarma <Vikasvk17@outlook.com>
2024-12-29 12:20:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,48 @@
+# S3 Bucket
+resource "aws_s3_bucket" "example" {
+  bucket = var.s3_bucket_name
+}
+
+# EC2 Instance
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "ExampleEC2"
+  }
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "example" {
+  origin {
+    domain_name = aws_s3_bucket.example.bucket_regional_domain_name
+    origin_id   = "S3Origin"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3Origin"
+
+    forwarded_values {
+      query_string = false
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
